c3dfd98bd1721b465c0a0ac9a93f4220745aeb80
Camila Lazinski <77087519+clazinski@users.noreply.github.com>
2021-07-01 21:50:27 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,22 +3,49 @@
 
 void mensagem()
 {
-  printf("Eu sou um programa escrito em C com id %d\n", getpid());
-  printf("Meu pai é o processo %d\n\n", getppid());
+  printf("ID FILHO %d - ", getpid());
+  printf("ID PAI %d\n", getppid());
 }
 
 int main()
 {
-  int valor = fork();
-  
-  if (valor == 0) {    // Processo filho
+  int pid = fork();
+  if (pid == 0)
+  {
+    printf("Primeiro filho\n");
     mensagem();
-    printf("Eu sou o filho e o valor de fork eh %d\n", valor);
   }
-  else if (valor > 0) {  // Processo pai
-    mensagem();
-    wait();   // Faz com que o pai aguarde a execução do processo filho
-    printf("Meu filho eh o processo %d\n", valor);
+  else
+  {
+    wait();
+    pid = fork();
+    wait();
+    if (pid == 0)
+    {
+      printf("\nSegundo filho\n");
+      for(int i = 1; i < 11; i++){
+        printf("Execucao %d\n", i);
+        mensagem();
+      }
+      
+    }
+    else
+    {
+      pid = fork();
+      wait();
+      if (pid == 0)
+      {
+        printf("\nTerceiro filho\n");
+        mensagem();
+        int a;
+        int b;
+        printf("Digite um numero inteiro\n");
+        scanf("%i", &a);
+         printf("Digite um numero inteiro\n");
+        scanf("%i", &b);
+        printf("Soma: %d\n", a + b);
+      }
+    }
   }
-  return 0;  
+  return 0;
 }
